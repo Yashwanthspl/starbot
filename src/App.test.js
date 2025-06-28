126c829c0d2fd558b437d7ca9e441fc5698ef04e
Yashwanth <yashwanthlsp88@gmail.com>
2025-06-28 11:48:27 +0530

    An example for preview homepage css and js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Webcafe AI welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/Welcome to Webcafe AI/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
